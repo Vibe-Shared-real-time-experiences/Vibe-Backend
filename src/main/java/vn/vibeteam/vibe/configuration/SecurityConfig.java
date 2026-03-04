09197ec1a5bca68a77586b5d9286e50e0077f136
hthinh24 <hungthinh240305@gmail.com>
2026-03-04 09:24:04 +0700

    update cors config

--- src/main/java/vn/vibeteam/vibe/configuration/SecurityConfig.java
+++ src/main/java/vn/vibeteam/vibe/configuration/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        configuration.setAllowedOrigins(List.of(allowedOrigins));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Allow cookies/auth headers
